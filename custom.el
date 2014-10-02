cef2c23757258dffe9c5f4f3488e9b71b5b3eec7
Samuel Tonini <tonini.samuel@gmail.com>
2014-10-02 20:43:44 +0200

    update display setup

--- custom.el
+++ custom.el
@@ -4,7 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cabbage-completion-framework (quote auto-complete))
- '(cabbage-org-files "~/Dropbox/org/")
  '(completion-ignored-extensions
    (quote
     (".rbc" ".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" "node_modules/" "tmp/")))
--- private/display.el
+++ private/display.el
@@ -2,8 +2,11 @@
 (if (fboundp 'blink-cursor-mode)
     (blink-cursor-mode 0))
 
-;; (set-frame-font "-apple-Source_Code_Pro-medium-r-normal--13-130-72-72-m-130-iso10646-1")
-(set-frame-font "-apple-Menlo-medium-r-normal--13-130-72-72-m-130-iso10646-1")
+(setq visible-bell nil)
+
+(menu-bar-mode -1)
+
+(set-frame-font "Ubuntu Mono-13")
 
 (set-frame-parameter nil 'fullscreen 'fullboth)
 (setq default-cursor-type 'box)
