a76be508888beb20adbcd9bd9910c664fb1061d0
Samuel Tonini <tonini.samuel@gmail.com>
2014-11-05 07:18:34 +0100

    add custom hook for testing and pairs

--- bundles/tonini-elixir/bundle.el
+++ bundles/tonini-elixir/bundle.el
@@ -3,3 +3,11 @@
 
 (require 'elixir-mode)
 (require 'alchemist)
+
+(defun cabbage-elixir-mode-hook ()
+  (cabbage--set-pairs '("(" "{" "[" "\"" "\'" "|"))
+
+  (when (and buffer-file-name (string-match "_test.exs$" buffer-file-name))
+    (setq cabbage-testing-execute-function 'alchemist-mix-test-file)))
+
+(add-hook 'elixir-mode-hook 'cabbage-elixir-mode-hook)
