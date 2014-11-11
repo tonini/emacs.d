0747004244a7cd4fc1a8d0a5bd4b081f5b7137d8
Samuel Tonini <tonini.samuel@gmail.com>
2014-11-11 06:42:57 +0100

    custom prefixes for help and emacs lisp functionality

--- bundles/tonini-bindings/bundle.el
+++ bundles/tonini-bindings/bundle.el
@@ -1,23 +1,33 @@
 
 (cabbage-global-set-key (kbd "C-c c") 'mode-compile)
 (cabbage-global-set-key (kbd "C-c k") 'mode-compile-kill)
-;; ruby compilation
 (cabbage-global-set-key (kbd "C-x M-r") 'ruby-compilation-this-buffer)
-
-;; buffer handling
 (cabbage-global-set-key (kbd "C-x M-k") 'kill-this-buffer)
-
-;; ace jump mode
-;; (cabbage-global-set-key (kbd "M-,") 'yas/expand)
-
-;; dirtree
 (cabbage-global-set-key (kbd "C-x t") 'dired-other-window)
-
-;; Popwin
 (cabbage-global-set-key (kbd "C-z") popwin:keymap)
 
-;; Personal Prefix M-s "Master - Sam" ;-)
-(define-prefix-command 's-map)
-(global-set-key (kbd "M-,") 's-map)
+;; Help
 
-(cabbage-global-set-key (kbd "M-, g") 'rgrep)
+(defvar lisp-find-map)
+(define-prefix-command 'lisp-find-map)
+
+(cabbage-global-set-key (kbd "C-h e") 'lisp-find-map)
+
+(cabbage-global-set-key (kbd "C-h e c") 'finder-commentary)
+(cabbage-global-set-key (kbd "C-h e e") 'view-echo-area-messages)
+(cabbage-global-set-key (kbd "C-h e f") 'find-function)
+(cabbage-global-set-key (kbd "C-h e F") 'find-face-definition)
+(cabbage-global-set-key (kbd "C-h e i") 'info-apropos)
+(cabbage-global-set-key (kbd "C-h e k") 'find-function-on-key)
+(cabbage-global-set-key (kbd "C-h e l") 'find-library)
+
+;; Emacs Lisp
+
+(defvar lisp-eval-map)
+(define-prefix-command 'lisp-eval-map)
+
+(cabbage-global-set-key (kbd "C-c e") 'lisp-eval-map)
+
+(cabbage-global-set-key (kbd "C-c e b") 'eval-buffer)
+(cabbage-global-set-key (kbd "C-c e r") 'eval-region)
+(cabbage-global-set-key (kbd "C-c e l") 'eval-last-sexp)
