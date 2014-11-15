00721f8c355ad19f528324bd04a5b0831f19efd1
Samuel Tonini <tonini.samuel@gmail.com>
2014-11-15 07:45:20 +0100

    update elixir bundle

--- bundles/tonini-elixir/bundle.el
+++ bundles/tonini-elixir/bundle.el
@@ -1,8 +1,22 @@
 (add-to-list 'load-path "~/Projects/emacs-elixir/")
 (add-to-list 'load-path "~/Projects/alchemist.el/")
 
+(cabbage-vendor 'textmate)
+
 (require 'elixir-mode)
 (require 'alchemist)
+
+;; add additional files / directories to execlude from textmate-goto-file
+(when (not (string-match "deps" *textmate-gf-exclude*))
+  (setq *textmate-gf-exclude*
+        (replace-regexp-in-string "build"
+                                  "build|_build|cover|deps"
+                                  *textmate-gf-exclude*))
+
+  (setq *textmate-project-roots*
+        (append *textmate-project-roots* '("mix.exs"))))
+
+(add-hook 'elixir-mode-hook 'cabbage-elixir-mode-hook)
 
 (defun cabbage-elixir-mode-hook ()
   (cabbage--set-pairs '("(" "{" "[" "\"" "\'" "|"))
