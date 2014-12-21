47fa3d69c90d41a67da8822d8db0b65048e0b851
Samuel Tonini <tonini.samuel@gmail.com>
2014-12-21 20:52:21 +0100

    remove old config

--- bundles/tonini-js/bundle.el
+++
@@ -1,5 +0,0 @@
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-
-(add-to-list 'load-path "~/Projects/karma.el/")
-
-(require 'karma)
--- init.el
+++
@@ -1,3 +0,0 @@
-(setq cabbage-repository (expand-file-name "/Users/tonini/Projects/cabbage/"))
-(load (concat cabbage-repository "cabbage"))
-(put 'ido-exit-minibuffer 'disabled nil)
