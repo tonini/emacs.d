8a8b7290c8349f6fd4c18951035b3bc6b96a2b36
Samuel Tonini <tonini.samuel@gmail.com>
2014-12-22 08:48:53 +0100

    update ruby part

--- parts/ruby-prt.el
+++ parts/ruby-prt.el
@@ -1,3 +1,7 @@
+(require 'ruby-mode)
+(require 'inf-ruby)
+(require 'ruby-compilation)
+
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
