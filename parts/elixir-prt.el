07f843cbf69138ab4c4214af502a6fb51dbfa6cb
Samuel Tonini <tonini.samuel@gmail.com>
2015-01-02 10:10:10 +0100

    setup elixir part to work with tester.el

--- parts/elixir-prt.el
+++ parts/elixir-prt.el
@@ -5,7 +5,9 @@
 (require 'alchemist)
 
 (defun t-elixir-mode-hook ()
-  (flycheck-mode +1))
+  (flycheck-mode +1)
+  (tester-init-test-run #'alchemist-mix-test-file "_test.exs$")
+  (tester-init-test-suite-run #'alchemist-mix-test))
 
 (add-hook 'elixir-mode-hook  't-elixir-mode-hook)
 
