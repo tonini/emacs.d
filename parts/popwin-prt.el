d4c0d2884b124affa642157f9128a96c2d8e5cd2
Samuel Tonini <tonini.samuel@gmail.com>
2015-01-09 21:43:22 +0100

    change popwin buffer heights

--- parts/popwin-prt.el
+++ parts/popwin-prt.el
@@ -17,9 +17,10 @@
 (push "*Kill Ring*" popwin:special-display-config)
 (push '(grep-mode :noselect t :stick t) popwin:special-display-config)
 (push '("*Messages*" :noselect t :stick t) popwin:special-display-config)
-(push '("*elixir help*" :noselect t :stick t) popwin:special-display-config)
+(push '("*elixir help*" :height 25 :noselect t :stick t) popwin:special-display-config)
 (push '("*compilation*" :noselect t :stick t) popwin:special-display-config)
-(push '("*mix*" :noselect t :stick t) popwin:special-display-config)
+(push '("*mix*" :height 25 :noselect t) popwin:special-display-config)
+(push '("*alchemist message*" :height 20 :noselect t) popwin:special-display-config)
 (push '("*overseer*" :noselect t :stick t) popwin:special-display-config)
 
 (provide 'popwin-prt)
