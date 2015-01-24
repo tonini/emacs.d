c646559db7c778bbd414934ed64958b8da1cb4f9
Samuel Tonini <tonini.samuel@gmail.com>
2015-01-24 07:50:26 +0100

    failing header

--- parts/completion-prt.el
+++ parts/completion-prt.el
@@ -1,3 +1,12 @@
+;;; completion-prt.el --- completion setup
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(require 'company)
+
 (setq company-idle-delay 0.5)
 (setq company-tooltip-limit 10)
 (setq company-minimum-prefix-length 2)
@@ -6,3 +15,5 @@
 (global-company-mode 1)
 
 (provide 'completion-prt)
+
+;;; completion-prt.el ends here
