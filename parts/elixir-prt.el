fe2a38a3531a86f775301c478dd6444c94dcf258
Samuel Tonini <tonini.samuel@gmail.com>
2015-06-05 23:29:47 +0200

    update elixir-prt

--- parts/elixir-prt.el
+++ parts/elixir-prt.el
@@ -7,19 +7,29 @@
 (setq alchemist-goto-elixir-source-dir "~/Projects/elixir/")
 (setq alchemist-goto-erlang-source-dir "~/Projects/otp_src_17.4/")
 
+(eval-after-load 'smartparens
+  '(progn
+     (defun my-elixir-do-end-close-action (id action context)
+       (when (eq action 'insert)
+         (newline-and-indent)
+         (previous-line)
+         (indent-according-to-mode)))
+
+     (sp-with-modes '(elixir-mode)
+       (sp-local-pair "do" "end"
+                      :when '(("SPC" "RET"))
+                      :post-handlers '(:add my-elixir-do-end-close-action)
+                      :actions '(insert)))))
+
 (defun t-elixir-mode-hook ()
   (yas/minor-mode +1)
   (smartparens-mode +1)
   (tester-init-test-run #'alchemist-mix-test-file "_test.exs$")
   (tester-init-test-suite-run #'alchemist-mix-test))
 
-(defun custom-alchemist-hook ()
-  (set (make-local-variable 'company-backends) '(alchemist-company)))
-
 (defun t-erlang-mode-hook ()
   (define-key erlang-mode-map (kbd "M-,") 'alchemist-goto-jump-back))
 
-(add-hook 'alchemist-mode-hook  'custom-alchemist-hook)
 (add-hook 'elixir-mode-hook  't-elixir-mode-hook)
 (add-hook 'erlang-mode-hook 't-erlang-mode-hook)
 
