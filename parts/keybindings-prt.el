180b8e19d333ba626cb1bbe168cfb69aeac95ad8
Samuel Tonini <tonini.samuel@gmail.com>
2015-06-08 21:29:48 +0200

    fixing the arrow keybindings <3

--- parts/keybindings-prt.el
+++ parts/keybindings-prt.el
@@ -200,6 +200,16 @@
 ;; Popwin
 (global-set-key (kbd "C-y") popwin:keymap)
 
+;; Fix the arrow keys
+(if (not window-system) ;; Only use in tty-sessions.
+    (progn
+      (defvar arrow-keys-map (make-sparse-keymap) "Keymap for arrow keys")
+      (define-key esc-map "O" arrow-keys-map)
+      (define-key arrow-keys-map "A" 'previous-line)
+      (define-key arrow-keys-map "B" 'next-line)
+      (define-key arrow-keys-map "C" 'forward-char)
+      (define-key arrow-keys-map "D" 'backward-char)))
+
 (provide 'keybindings-prt)
 
 ;;; keybindings-prt.el ends here
