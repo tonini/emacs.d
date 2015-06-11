6cc02e51aa0161dba296ee20247e4269fe06cd24
Samuel Tonini <tonini.samuel@gmail.com>
2015-06-11 08:48:52 +0200

    ignore vendor/bundle gems

--- customize.el
+++ customize.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(projectile-globally-ignored-directories
    (quote
-    (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" "_build" ".cask" "deps" "node_modules" "lib/elixir/_build"))))
+    ("vendor/bundle" ".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" "_build" ".cask" "deps" "node_modules" "lib/elixir/_build"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
