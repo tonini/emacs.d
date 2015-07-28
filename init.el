880a76cf73816d2105f7d746904e14d4eff15565
Samuel Tonini <tonini.samuel@gmail.com>
2015-07-28 06:58:36 +0200

    no need to use pallet anymore

--- init.el
+++ init.el
@@ -11,9 +11,6 @@
 (require 'cask "/usr/local/share/emacs/site-lisp/cask.el")
 (cask-initialize)
 
-(require 'pallet)
-(pallet-mode t)
-
 (add-to-list 'load-path (expand-file-name "parts" user-emacs-directory))
 
 (require 'programming-prt)
