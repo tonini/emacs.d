12ea3ea91c9b095c5531e8266ab65a621762f51c
Samuel Tonini <tonini.samuel@gmail.com>
2015-07-30 08:54:17 +0200

    customize helm faces

--- customize.el
+++ customize.el
@@ -14,4 +14,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(helm-selection ((t (:background "color-237"))))
+ '(helm-source-header ((t (:background "color-233" :foreground "green" :weight bold))))
+ '(helm-visible-mark ((t (:background "brightred" :foreground "white")))))
