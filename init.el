0f9d246eb874f364d6a3c7dcd053ad6cf6c98e87
Samuel Tonini <tonini.samuel@gmail.com>
2015-09-07 13:52:27 +0200

    Add license information

--- init.el
+++ init.el
@@ -1,7 +1,29 @@
 ;;; init.el --- Personal emacs configuration of Samuel Tonini
 
-;;; Commentary:
+;; Copyright © 2014-2015 Samuel Tonini
 ;;
+;; Author: Samuel Tonini <tonini.samuel@gmail.com>
+;; Maintainer: Samuel Tonini <tonini.samuel@gmail.com>
+;; URL: http://www.github.com/tonini/emacs.d
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Personal emacs configuration of Samuel Tonini
 
 ;;; Code:
 
