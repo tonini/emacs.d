fa67b712347c5f38fe2c897041863a15c241c13c
Samuel Tonini <tonini.samuel@gmail.com>
2015-11-11 07:15:09 +0100

    Use a more readable foreground helm selection color

--- customize.el
+++ customize.el
@@ -14,6 +14,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(helm-selection ((t (:background "gray" :foreground "black"))))
+ '(helm-selection ((t (:background "cyan" :foreground "color-16"))))
  '(helm-source-header ((t (:background "color-233" :foreground "green" :weight bold))))
  '(helm-visible-mark ((t (:background "brightred" :foreground "white")))))
